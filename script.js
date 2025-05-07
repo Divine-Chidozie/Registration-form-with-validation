8d40a68f77a63ab546a897fcb08167647bce471b
Divine Chidozie <igwedivine92@gmail.com>
2025-05-06 17:59:21 -0700

    Registration form with validation

--- script.js
+++ script.js
@@ -23,30 +23,64 @@ const termsAndConditionsError = document.getElementById(
   "terms-and-conditions-error"
 );
 
+const profilePicture = document.getElementById("profile-picture");
+const profilePictureError = document.getElementById("profile-picture-error");
+
 const successMessage = document.getElementById("success-message");
 
+const age = document.getElementById("age");
+const ageError = document.getElementById("age-error");
+const newAge = "please enter your age";
+
+const referral = document.getElementById("referral");
+const referralError = document.getElementById("referral-option");
+
+const bio = document.getElementById("bio");
+const bioError = document.getElementById("bio-error");
 // clear error on input
-[firstName, lastName, email, newPassword].forEach((input, i) => {
+[
+  firstName,
+  lastName,
+  email,
+  newPassword,
+  profilePicture,
+  age,
+  referral,
+  bio,
+].forEach((input, i) => {
   const errorElements = [
     firstNameError,
     lastNameError,
     emailError,
     newPasswordError,
+    profilePictureError,
+    ageError,
+    referralError,
+    bioError,
   ];
   input.addEventListener("input", () => {
     errorElements[i].textContent = "";
-    successMessage.textContent = ""; // clear success message when editing
   });
 });
+
+personalAccount.addEventListener("change", () => {
+  businessAccountError.textContent = "";
+});
+businessAccount.addEventListener("change", () => {
+  businessAccountError.textContent = "";
+});
+termsAndConditions.addEventListener("change", () => {
+  termsAndConditionsError.textContent = "";
+});
+
+const errorColor = "red";
+const errorFontSize = "13px";
 
 // adding eventlisterner to form
 form.addEventListener("submit", (e) => {
   e.preventDefault(); // prevent form from submitting
 
   let isValid = true;
-
-  const errorColor = "red";
-  const errorFontSize = "14px";
 
   // form validation
   if (!firstName.value.trim()) {
@@ -65,7 +99,7 @@ form.addEventListener("submit", (e) => {
   if (!email.value.trim()) {
     emailError.textContent = "please enter your email address";
     emailError.style.color = errorColor;
-    email.style.fontSize = errorFontSize;
+    emailError.style.fontSize = errorFontSize;
     isValid = false;
   } else if (!emailPattern.test(email.value.trim())) {
     emailError.textContent = "please enter a valid email address";
@@ -74,27 +108,72 @@ form.addEventListener("submit", (e) => {
     isValid = false;
   }
   if (!newPassword.value.trim()) {
-    newPasswordError.textContent = " please enter a new password";
+    newPasswordError.textContent = "please enter a new password";
     newPasswordError.style.color = errorColor;
     newPasswordError.style.fontSize = errorFontSize;
     isValid = false;
-  } else if (newPassword.value.trim().length <= 7) {
+  } else if (newPassword.value.trim().length < 8) {
     newPasswordError.textContent =
-      "password must have a minimum of 7 characters";
+      "password must have a minimum of 8 characters";
+    isValid = false;
+  }
+  if (!personalAccount.checked && !businessAccount.checked) {
+    businessAccountError.textContent = "you must choose an account type";
+    businessAccountError.style.color = errorColor;
+    businessAccountError.style.fontSize = errorFontSize;
+    businessAccountError.style.margin = "0px";
+    isValid = false;
+  } else {
+    businessAccountError.textContent = "";
+  }
+  if (!termsAndConditions.checked) {
+    termsAndConditionsError.textContent =
+      "You must accept the Terms and Conditions";
+    termsAndConditionsError.style.color = errorColor;
+    termsAndConditionsError.style.fontSize = errorFontSize;
+    termsAndConditionsError.style.margin = "0px";
+    isValid = false;
+  }
+  if (!profilePicture.files.length) {
+    profilePictureError.textContent = "please upload a profile picture";
+    profilePictureError.style.color = errorColor;
+    profilePictureError.style.fontSize = errorFontSize;
     isValid = false;
   }
 
-  // if all fields are valid
+  if (!age.value.trim()) {
+    ageError.textContent = newAge;
+    ageError.style.color = errorColor;
+    ageError.style.fontSize = errorFontSize;
+    isValid = false;
+  } else if (parseInt(age.value) < 18) {
+    ageError.textContent = "you must be 18+ proceed";
+    ageError.style.color = "yellow";
+    ageError.style.fontSize = errorFontSize;
+    isValid = false;
+  } else if (age.value >= 18) {
+    console.log("welcome user");
+  }
+  if (referral.value === "") {
+    referralError.textContent = "please select an option";
+    referralError.style.color = errorColor;
+    referralError.style.fontSize = errorFontSize;
+    isValid = false;
+  }
+
+  if (bio.value.trim() === "") {
+    bioError.textContent = "Textarea cannot be empty.";
+    bioError.style.color = errorColor;
+    bioError.style.fontSize = errorFontSize;
+    isValid = false;
+  }
+
   if (isValid) {
+    // if all fields are valid
     successMessage.textContent = "Form submitted successfully";
     successMessage.style.color = "green";
     successMessage.style.fontSize = "15px";
     successMessage.style.textAlign = "center";
     successMessage.style.marginBottom = "2rem";
   }
-
-  setTimeout(() => {
-    form.reset();
-    successMessage.textContent = "";
-  }, 3000);
 });
